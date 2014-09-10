694bf9960da6187151e25f9d6dc01fa93a53dc39
Michael Heard <compmike@gmail.com>
2014-09-10 18:32:58 -0400

    added themoviedb

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'coffee-rails', '~> 4.0.0'
 gem 'devise'
 gem 'tzinfo-data'
 gem 'autoprefixer-rails'
+gem 'themoviedb'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer',  platforms: :ruby
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
+  Tmdb::Api.key("38add3c585699220139ff98bdc896257")
   # Do not eager load code on boot.
   config.eager_load = false
 
